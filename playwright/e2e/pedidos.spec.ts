9176a8cbfa2cedede438dece6055a6d2f565d83a
DieroKreator <qtechwired@gmail.com>
2026-02-04 10:14:51 -0300

    Add timeout on config

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -1,21 +1,20 @@
 import { test, expect } from '@playwright/test'
 
 test('deve consultar um pedido aprovado', async ({ page }) => {
-  await page.goto('http://localhost:5173/')
+    // Arrange
+    await page.goto('http://localhost:5173/')
+    await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
+    await page.getByRole('link', { name: 'Consultar Pedido' }).click()
+    await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
 
-  // Checkpoint
-  await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
+    // Act
+    await page.getByTestId('search-order-id').fill('VLO-6E2J20')
+    await page.getByTestId('search-order-button').click()
 
-  await page.getByRole('link', { name: 'Consultar Pedido' }).click()
+    // Assert
+    expect(page.getByTestId('order-result-id')).toBeVisible()
+    expect(page.getByTestId('order-result-id')).toContainText('VLO-6E2J20')
 
-  await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
-
-  await page.getByTestId('search-order-id').fill('VLO-6E2J20')
-  await page.getByTestId('search-order-button').click()
-
-  expect(page.getByTestId('order-result-id')).toBeVisible()
-  expect(page.getByTestId('order-result-id')).toContainText('VLO-6E2J20')
-
-  expect(page.getByTestId('order-result-status')).toBeVisible()
-  expect(page.getByTestId('order-result-status')).toContainText('APROVADO')
+    expect(page.getByTestId('order-result-status')).toBeVisible()
+    expect(page.getByTestId('order-result-status')).toContainText('APROVADO')
 })
