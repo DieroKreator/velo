c4acdfc61eea9f2387969b625337fb79e789ed05
DieroKreator <qtechwired@gmail.com>
2026-02-11 18:56:49 -0300

    Verify through snapshot

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -23,16 +23,45 @@ test.describe('Consulta de Pedido', ()=> {
         // Assert
         // expect(page.getByTestId('order-result-id')).toContainText(order)
     
-        const containerPedido = page.getByRole('paragraph')
-            .filter({ hasText: /^Pedido$/ }) //Aplicar regra de busqueda
-            .locator('..') //Sobe um nivel e pega o pai do pedido
-        await expect(containerPedido).toContainText(order, { timeout: 10_000 })
+        // const containerPedido = page.getByRole('paragraph')
+        //     .filter({ hasText: /^Pedido$/ }) //Aplicar regra de busqueda
+        //     .locator('..') //Sobe um nivel e pega o pai do pedido
+        // await expect(containerPedido).toContainText(order, { timeout: 10_000 })
     
-        // const orderCode = page.locator('//p[text()="Pedido"]/..//p[text()="VLO-GXNL53"]')
-        // await expect(orderCode).toBeVisible({ timeout: 10_000 })
+        // // const orderCode = page.locator('//p[text()="Pedido"]/..//p[text()="VLO-GXNL53"]')
+        // // await expect(orderCode).toBeVisible({ timeout: 10_000 })
     
-        // expect(page.getByTestId('order-result-status')).toContainText('APROVADO')
-        await expect(page.getByText('APROVADO')).toBeVisible()
+        // // expect(page.getByTestId('order-result-status')).toContainText('APROVADO')
+        // await expect(page.getByText('APROVADO')).toBeVisible()
+
+        await expect(page.getByTestId(`order-result-${order}`)).toMatchAriaSnapshot(`
+            - img
+            - paragraph: Pedido
+            - paragraph: ${order}
+            - img
+            - text: APROVADO
+            - img "Velô Sprint"
+            - paragraph: Modelo
+            - paragraph: Velô Sprint
+            - paragraph: Cor
+            - paragraph: Glacier Blue
+            - paragraph: Interior
+            - paragraph: cream
+            - paragraph: Rodas
+            - paragraph: aero Wheels
+            - heading "Dados do Cliente" [level=4]
+            - paragraph: Nome
+            - paragraph: Pepe Cannavaro
+            - paragraph: Email
+            - paragraph: peca@dev.co
+            - paragraph: Loja de Retirada
+            - paragraph
+            - paragraph: Data do Pedido
+            - paragraph: /\\d+\\/\\d+\\/\\d+/
+            - heading "Pagamento" [level=4]
+            - paragraph: À Vista
+            - paragraph: /R\\$ \\d+\\.\\d+,\\d+/
+            `);
     })
     
     test('deve exibir mensagem quando o pedido não', async ({ page }) => {
