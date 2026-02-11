541f89e982e10890b9e314811bed3d8f2127ab30
DieroKreator <qtechwired@gmail.com>
2026-02-11 10:51:50 -0300

    Improve findElement startegy for orderCode and DDT

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -2,18 +2,30 @@ import { test, expect } from '@playwright/test'
 
 test('deve consultar um pedido aprovado', async ({ page }) => {
 
+    // Test Data
+    const order = 'VLO-GXNL53'
+
+    // Arrange
     await page.goto('http://localhost:5173/')
     await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
     await page.getByRole('link', { name: 'Consultar Pedido' }).click()
     await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
 
-    await page.getByRole('textbox', { name: 'Número do pedido' }).fill('VLO-GXNL53') // Way more readable
+    // Act
+    await page.getByRole('textbox', { name: 'Número do pedido' }).fill(order) // Way more readable
     await page.getByRole('button', { name: 'Buscar Pedido' }).click()
 
     // Assert
-    await expect(page.getByText('VLO-GXNL53')).toBeVisible({ timeout: 10_000 })
-    // expect(page.getByTestId('order-result-id')).toContainText('VLO-6E2J20')
+    // expect(page.getByTestId('order-result-id')).toContainText(order)
 
-    await expect(page.getByText('APROVADO')).toBeVisible()
+    const containerPedido = page.getByRole('paragraph')
+        .filter({ hasText: /^Pedido$/ }) //Aplicar regra de busqueda
+        .locator('..') //Sobe um nivel e pega o pai do pedido
+    await expect(containerPedido).toContainText(order, { timeout: 10_000 })
+
+    // const orderCode = page.locator('//p[text()="Pedido"]/..//p[text()="VLO-GXNL53"]')
+    // await expect(orderCode).toBeVisible({ timeout: 10_000 })
+
     // expect(page.getByTestId('order-result-status')).toContainText('APROVADO')
+    await expect(page.getByText('APROVADO')).toBeVisible()
 })
