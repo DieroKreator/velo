2c57dc986cb30a0c875623b29dbf13a1258bdf93
DieroKreator <qtechwired@gmail.com>
2026-04-07 15:59:59 -0300

    Add validateOrderDetails() to optimize code

--- playwright/support/pages/OrderLockupPage.ts
+++ playwright/support/pages/OrderLockupPage.ts
@@ -3,7 +3,7 @@ import { Page, expect } from '@playwright/test'
 type OrderStatus = 'APROVADO' | 'REPROVADO' | 'EM_ANALISE'
 
 export class OrderLockupPage {
-  constructor(private page: Page) {}
+  constructor(private page: Page) { }
 
   async searchOrder(code: string) {
     await this.page.getByRole('textbox', { name: 'Número do Pedido' }).fill(code)
@@ -12,7 +12,7 @@ export class OrderLockupPage {
 
   async validateStatusBadge(status: OrderStatus) {
     const statusClasses = {
-      APROVADO:  {
+      APROVADO: {
         background: 'bg-green-100',
         text: 'text-green-700',
         icon: 'lucide-circle-check-big'
@@ -35,5 +35,37 @@ export class OrderLockupPage {
     await expect(statusBadge).toHaveClass(new RegExp(classes.background))
     await expect(statusBadge).toHaveClass(new RegExp(classes.text))
     await expect(statusBadge.locator('svg')).toHaveClass(new RegExp(classes.icon))
+  }
+
+  async validateOrderDetails(order: any) {
+    await expect(this.page.getByTestId(`order-result-${order.number}`)).toMatchAriaSnapshot(`
+      - img
+      - paragraph: Pedido
+      - paragraph: ${order.number}
+      - status:
+        - img
+        - text: ${order.status}
+      - img "Velô Sprint"
+      - paragraph: Modelo
+      - paragraph: Velô Sprint
+      - paragraph: Cor
+      - paragraph: ${order.color}
+      - paragraph: Interior
+      - paragraph: cream
+      - paragraph: Rodas
+      - paragraph: ${order.wheels}
+      - heading "Dados do Cliente" [level=4]
+      - paragraph: Nome
+      - paragraph: ${order.customer.name}
+      - paragraph: Email
+      - paragraph: ${order.customer.email}
+      - paragraph: Loja de Retirada
+      - paragraph
+      - paragraph: Data do Pedido
+      - paragraph: /\\d+\\/\\d+\\/\\d+/
+      - heading "Pagamento" [level=4]
+      - paragraph: ${order.payment}
+      - paragraph: /R\\$ \\d+\\.\\d+,\\d+/
+      `)
   }
 }
