4381c4add57b59011c737ae22e25f7c4bf9e3d09
DieroKreator <qtechwired@gmail.com>
2026-05-16 18:27:52 -0300

    Update import and remove function

--- playwright/support/database/orderRepository.ts
+++ playwright/support/database/orderRepository.ts
@@ -1,7 +1,7 @@
 import { db } from './database'
 import { OrderTable } from './schema'
 
-import { OrderDetails } from '../actions/orderLockupActions'
+import { OrderDetails } from '../actions/orderLookupActions'
 
 import crypto from 'crypto'
 
@@ -39,8 +39,4 @@ export async function insertOrder(order: OrderDetails) {
 
 export async function deleteOrderByNumber(orderNumber: string) {
   await db.deleteFrom('orders').where('order_number', '=', orderNumber).execute()
-}
-
-export async function deleteOrderByEmail(email: string) {
-  await db.deleteFrom('orders').where('customer_email', '=', email).execute()
 }
