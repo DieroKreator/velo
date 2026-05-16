0e6b72335f2dfc75a09eb4ed51a875c9f6d37021
DieroKreator <qtechwired@gmail.com>
2026-05-16 18:22:50 -0300

    Update naming

--- playwright/support/fixtures.ts
+++ playwright/support/fixtures.ts
@@ -1,17 +1,17 @@
 import { test as base } from '@playwright/test'
 
-import { createOrderLockupActions } from './actions/orderLockupActions'
+import { createOrderLookupActions } from './actions/orderLookupActions'
 import { createConfiguratorActions } from './actions/configuratorActions'
 
 type App = {
-  orderLockup: ReturnType<typeof createOrderLockupActions>
+  orderLookup: ReturnType<typeof createOrderLookupActions>
   configurator: ReturnType<typeof createConfiguratorActions>
 }
 
 export const test = base.extend<{ app: App }>({
   app: async ({ page }, use) => {
     const app: App = {
-      orderLockup: createOrderLockupActions(page),
+      orderLookup: createOrderLookupActions(page),
       configurator: createConfiguratorActions(page)
     }
     await use(app)
